0327c3f7203129336de949bb476bd35c8216f068
kcowes <kcowes@uade.edu.ar>
2025-04-22 12:59:00 -0300

    commit que te salvo el orto - menu de hamb y logo centrado

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     const toggleButton = document.querySelector('.nav__toggle');
-    const closeButton = document.querySelector('.nav__close');
     const menu = document.querySelector('.nav__link');
 
-    toggleButton.addEventListener('click', function () {
-        menu.classList.toggle('show');
-    });
-
-    closeButton.addEventListener('click', function () {
-        menu.classList.remove('show');
-    });
     
+    toggleButton.addEventListener('click', function () {
+        menu.classList.toggle('nav__link--show');
+    });
 });
